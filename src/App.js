fff330fa33e264fed5799f0d10d6629ebf935234
Manjunadh <manjunadhb@gmail.com>
2024-12-17 17:31:28 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pushpa 2</h1>
+      <h1>Pusha 1</h1>
+
+      <h2>Allu Arjun</h2>
+      <img src="./alluarjun/aaPoster.jpg"></img>
+      <img src="./rashmika/rashmikaPoster.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
